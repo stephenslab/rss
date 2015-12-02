23d524e000a2cc72244d96605c0b06ce949e7c1e
Xiang Zhu <xiangzhu.nku@gmail.com>
2015-12-01 18:54:18 -0600

    update git ignore file

--- examples/example1.m
+++ examples/example1.m
@@ -11,8 +11,8 @@ bwd = example_data.bwd; % bandwidth of R
 BR  = example_data.BR; 	% banded storage of R
 
 betahat = example_data.betahat; % single-SNP effect size estimates
-se = example_data.se; 		% standard error of betahat
-Nsnp = example_data.Nsnp; 	% sample size of each SNP
+se 	= example_data.se; 		% standard error of betahat
+Nsnp 	= example_data.Nsnp; 	% sample size of each SNP
 
 fprintf('Data set is loaded ... \n');
 
@@ -30,13 +30,13 @@ tic;
 % 1. simulate posterior samples via mcmc
 [betasam, gammasam, hsam, logpisam, Naccept] = rss_bvsr(betahat, se, R, Nsnp, Ndraw, Nburn, Nthin);
 % 2. compute the posterior samples of pve
-matrix_type = 1;
-M = length(hsam);
-pvesam = zeros(M,1);
-progress_bar = progress('init','start PVE calculation');
+matrix_type 	= 1;
+M 		= length(hsam);
+pvesam 		= zeros(M,1);
+progress_bar 	= progress('init','start PVE calculation');
 for i = 1:M
-	pvesam(i) = compute_pve(betasam(i,:), betahat, se, Nsnp, bwd, BR, matrix_type);
-	progress_bar = progress(progress_bar, i/M);
+	pvesam(i) 	= compute_pve(betasam(i,:), betahat, se, Nsnp, bwd, BR, matrix_type);
+	progress_bar 	= progress(progress_bar, i/M);
 end
 runtime = toc;
 % 3. save output
@@ -53,13 +53,13 @@ tic;
 % 1. simulate posterior samples via mcmc
 [bsam, zsam, lpsam, hsam, rsam, Naccept] = rss_bslmm(betahat, se, R, Nsnp, Ndraw, Nburn, Nthin);
 % 2. compute the posterior samples of pve
-matrix_type = 1;
-M = length(hsam);
-pvesam = zeros(M,1);
-progress_bar = progress('init','start PVE calculation');
+matrix_type 	= 1;
+M 		= length(hsam);
+pvesam 		= zeros(M,1);
+progress_bar 	= progress('init','start PVE calculation');
 for i = 1:M 
-        pvesam(i) = compute_pve(bsam(i,:), betahat, se, Nsnp, bwd, BR, matrix_type);
-        progress_bar = progress(progress_bar, i/M);
+        pvesam(i) 	= compute_pve(bsam(i,:), betahat, se, Nsnp, bwd, BR, matrix_type);
+        progress_bar 	= progress(progress_bar, i/M);
 end
 runtime = toc;
 % 3. save output
@@ -77,13 +77,13 @@ tic;
 % 1. simulate posterior samples via mcmc
 [bsam, zsam, wsam, lsam, Naccept] = rss_ash(betahat, se, R, Nsnp, sigma_beta, Ndraw, Nburn, Nthin);
 % 2. compute the posterior samples of pve
-matrix_type = 1;
-M = length(lsam);
-pvesam = zeros(M,1);
-progress_bar = progress('init','start PVE calculation');
+matrix_type 	= 1;
+M 		= length(lsam);
+pvesam 		= zeros(M,1);
+progress_bar 	= progress('init','start PVE calculation');
 for i = 1:M
-        pvesam(i) = compute_pve(bsam(i,:), betahat, se, Nsnp, bwd, BR, matrix_type);
-        progress_bar = progress(progress_bar, i/M);
+        pvesam(i) 	= compute_pve(bsam(i,:), betahat, se, Nsnp, bwd, BR, matrix_type);
+        progress_bar 	= progress(progress_bar, i/M);
 end
 runtime = toc;
 % 3. save output
@@ -91,3 +91,4 @@ save('example1_rssash.mat', 'bsam', 'zsam', 'wsam', 'lsam', 'pvesam', 'Naccept',
 clearvars bsam zsam wsam lsam pvesam Naccept runtime;
 fprintf('RSS-ASH analysis is done ... \n');
 
+exit;
