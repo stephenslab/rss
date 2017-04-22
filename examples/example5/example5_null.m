17f38dba157ba8c6e8beafbdc1170308cecbbb68
Xiang Zhu <xiangzhu.nku@gmail.com>
2017-04-22 11:55:11 -0500

    add example 5 null part

---
+++ examples/example5/example5_null.m
@@ -0,0 +1,43 @@
+% input data file: ibd2015_sumstat.mat
+% source: Liu, van Sommeren et al, 2015 (PUBMED: 26192919)
+
+% please name the summary data file as
+% "trait_name"_sumstat.mat
+% in order to run the following code
+
+% specify trait-specific information
+trait_name  = 'ibd2015';
+sample_size = (12882+21770); % cases: 12,882; controls: 21,770
+
+% specify grid of hyper-parameters
+h_rv      = 0.3;
+theta0_rv = (-2.9:0.025:-2.85);
+
+% specify stage of analysis
+stage = 'step1';
+
+% specify random start and algorithm 
+myseed = 459;
+method = 'squarem';
+
+% specify input and output paths
+input_path  = '/project/mstephens/test_rss/data/ibd2015/'; 
+output_path = './';
+
+%%%%%%%%%%%%% DO NOT MODIFY CODES BELOW %%%%%%%%%%%%%
+
+% add search path
+addpath('../../src_vb/');
+
+% create all combinations of two vectors
+% https://www.mathworks.com/help/nnet/ref/combvec.html
+hyper_para = combvec(h_rv, theta0_rv);
+
+% specify hyper-parameters based on job id 
+h      = hyper_para(1, case_id);
+theta0 = hyper_para(2, case_id);
+
+% run analyses
+run('null_template.m');
+
+exit;
