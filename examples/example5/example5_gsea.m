59308a90dc7a4c442a13f73b3d1b0a792f52336b
Xiang Zhu <xiangzhu.nku@gmail.com>
2017-04-22 11:55:29 -0500

    add example 5 enrichment part

---
+++ examples/example5/example5_gsea.m
@@ -0,0 +1,39 @@
+clear;
+
+% add rss-varbvsr to the search path
+addpath('../../src_vb');
+
+% fitting method and random seed
+stage  = 'step1';
+myseed = 459;
+method = 'squarem';
+
+% specify the gene set name
+% IL23-mediated signaling events (Pathway Commons 2, PID, 37 genes)
+geneset_name = 'path2641'; 
+
+% specify the path of summary data across whole genome
+allstat_path = '/project/mstephens/test_rss/data/ibd2015/ibd2015_sumstat.mat';
+
+% trait name and sample size
+trait_name  = 'ibd2015';
+sample_size = (12882+21770); % Table 1; cases: 12,882; controls: 21,770
+
+% parameter grids
+h      = 0.3;
+theta0 = (-2.9:0.025:-2.85)';
+theta  = (0:(3/100):3)';
+
+% specify the path of the results of null analysis
+data_path = './';
+
+% specify the path of subset of summary data corresponding to the gene set 
+sumstat_path = '/project/mstephens/test_rss/data/ibd2015/pathway/path2641/ibd2015-sumstat.mat';
+
+% specify the output path
+output_path = './'; 
+
+%===== MODIFICATION STOPS =====
+
+% run gsea on the gene set
+run('gsea_template.m') 
