b26201f4bce26b3d0d9af014f105f70091f17c0f
Xiang Zhu <xiangzhu.nku@gmail.com>
2018-07-26 13:20:54 -0500

    rmv type spec for R

--- src_vb/C/types.h
+++ src_vb/C/types.h
@@ -1,9 +1,21 @@
-// Author: Peter Carbonetto
-// Source: https://github.com/pcarbo/varbvs/blob/master/R/varbvs/src/types.h
+// This file is modified from the following file in varbvs package.
+// https://github.com/pcarbo/varbvs/blob/master/varbvs-R/src/types.h
+
+// Part of the varbvs package, https://github.com/pcarbo/varbvs
+//
+// Copyright (C) 2012-2017, Peter Carbonetto
+//
+// This program is free software: you can redistribute it under the
+// terms of the GNU General Public License; either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANY; without even the implied warranty of
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
 #ifndef INCLUDE_TYPES
 #define INCLUDE_TYPES
-
-#ifdef MATLAB_MEX_FILE
 
 // These type definitions are used to build the C routines for MATLAB.
 #include "matrix.h"
@@ -11,14 +23,5 @@
 typedef mwSize  Size;
 typedef mwIndex Index;
 typedef float   MatrixElem;
-
-#else
-
-// These type definitions are used to build the C routines for R. 
-typedef int    Size;
-typedef int    Index;
-typedef double MatrixElem;
-
-#endif
 
 #endif
