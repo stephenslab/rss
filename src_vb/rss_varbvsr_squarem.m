31c1985277ca2e6f37fb2372388c9f76d5c24ee7
Xiang Zhu <xiangzhu.nku@gmail.com>
2018-08-08 15:23:48 -0500

    fix the tab-space issue in display

--- src_vb/rss_varbvsr_squarem.m
+++ src_vb/rss_varbvsr_squarem.m
@@ -8,10 +8,10 @@ function [lnZ, alpha, mu, s, info] = rss_varbvsr_squarem(betahat, se, SiRiS, sig
 %       sigb: the prior SD of the regression coefficients (if included), scalar
 %       logodds: the prior log-odds (i.e. log(prior PIP/(1-prior PIP))) of inclusion for each SNP, p by 1 or scalar
 %       options: user-specified behaviour of the algorithm, structure
-%               - max_walltime: scalar, the maximum wall time (unit: seconds) for this program
-%               - tolerance: scalar, convergence tolerance
-%               - alpha & mu: p by 1 vectors, initial values of variational parameters
-%               - verbose: logical, print program progress if true
+%		- max_walltime: scalar, the maximum wall time (unit: seconds) for this program
+%		- tolerance: scalar, convergence tolerance
+%		- alpha & mu: p by 1 vectors, initial values of variational parameters
+%		- verbose: logical, print program progress if true
 %		- modify_step: logical, modify the step length in SQUAREM if true 
 % OUTPUT:
 %	lnZ: scalar, the variational lower bound of the marginal log likelihood (up to some constant)
@@ -134,30 +134,34 @@ function [lnZ, alpha, mu, s, info] = rss_varbvsr_squarem(betahat, se, SiRiS, sig
 
     % Run the first fix-point mapping step (line 1 of Table 1).
     [alpha1, mu1, SiRiSr] = rss_varbvsr_update(SiRiS, sigb, logodds, betahat, se, alpha0, mu0, SiRiSr, I); 
+    
     % Run the second fix-point mapping step (line 2 of Table 1).
     [alpha2, mu2, SiRiSr] = rss_varbvsr_update(SiRiS, sigb, logodds, betahat, se, alpha1, mu1, SiRiSr, I);
 
     % Compute the step length (line 3-5 of Table 1).
-    alpha_r 	= alpha1 - alpha0;
-    mu_r 	= mu1 - mu0;
-    alpha_v 	= (alpha2 - alpha1) - alpha_r;
-    mu_v 	= (mu2 - mu1) - mu_r;
-    mtp 	= - sqrt(norm(alpha_r)^2+norm(mu_r)^2) / sqrt(norm(alpha_v)^2+norm(mu_v)^2+eps); % add eps to avoid 0/0 case
+    alpha_r = alpha1 - alpha0;
+    mu_r    = mu1 - mu0;
+    alpha_v = (alpha2 - alpha1) - alpha_r;
+    mu_v    = (mu2 - mu1) - mu_r;
+    mtp     = - sqrt(norm(alpha_r)^2+norm(mu_r)^2) / sqrt(norm(alpha_v)^2+norm(mu_v)^2+eps); % add eps to avoid 0/0 case
 
     % Modifiy the step length (optional): three scenarios (Section 6).
     if modify_step
       % scenario 1: use the output of the second fix-point mapping output
-      if mtp >= -1 					 % set mtp = -1
+      % i.e. set mtp = -1
+      if mtp >= -1
         alpha3  = alpha2;
         mu3     = mu2;
         SiRiSr3 = SiRiSr;
-      % scenario 2: no need to modify the step length (line 7 of Table 1)  
-      else 						% i.e. mtp < -1
+      % scenario 2: no need to modify the step length (line 7 of Table 1)
+      % i.e. mtp < -1  
+      else
         alpha3  = alpha0 - 2*mtp*alpha_r + (mtp^2)*alpha_v;
         mu3     = mu0 - 2*mtp*mu_r + (mtp^2)*mu_v;
         SiRiSr3 = full(SiRiS * (alpha3 .* mu3));
       end
-    else % skip the modification of step length
+    % i.e. skip the modification of step length
+    else
       alpha3  = alpha0 - 2*mtp*alpha_r + (mtp^2)*alpha_v;
       mu3     = mu0 - 2*mtp*mu_r + (mtp^2)*mu_v;
       SiRiSr3 = full(SiRiS * (alpha3 .* mu3));
@@ -165,23 +169,29 @@ function [lnZ, alpha, mu, s, info] = rss_varbvsr_squarem(betahat, se, SiRiS, sig
 
     % Run the last fix-point mapping step for scenario 1 and 2 (line 8 of Table 1).
     [alpha, mu, SiRiSr] = rss_varbvsr_update(SiRiS, sigb, logodds, betahat, se, alpha3, mu3, SiRiSr3, I);
-    r 		  	= alpha .* mu;
-    lnZ 		= q'*r - 0.5*r'*SiRiSr - 0.5*(1./se_square)'*betavar(alpha, mu, s);
-    lnZ 		= lnZ + intgamma(logodds, alpha) + intklbeta_rssbvsr(alpha, mu, s, sigb_square);
+    
+    r   = alpha .* mu;
+    lnZ = q'*r - 0.5*r'*SiRiSr - 0.5*(1./se_square)'*betavar(alpha, mu, s);
+    lnZ = lnZ + intgamma(logodds, alpha) + intklbeta_rssbvsr(alpha, mu, s, sigb_square);
    
     % scenario 3: use a simple back-tracking to modify the steplength iteratively
     if modify_step && (mtp < -1) && (lnZ < lnZ0)
       num_bt = 0;
+      
+      % stop back-tracking after ten steps
       while (lnZ < lnZ0) && (num_bt < 10)
-        mtp 		    = 0.5*(mtp-1); % back-tracking
-    	alpha3              = alpha0 - 2*mtp*alpha_r + (mtp^2)*alpha_v;
-    	mu3                 = mu0 - 2*mtp*mu_r + (mtp^2)*mu_v;
-    	SiRiSr3             = full(SiRiS * (alpha3 .* mu3));
-    	[alpha, mu, SiRiSr] = rss_varbvsr_update(SiRiS, sigb, logodds, betahat, se, alpha3, mu3, SiRiSr3, I);
-    	r                   = alpha .* mu;
-    	lnZ                 = q'*r - 0.5*r'*SiRiSr - 0.5*(1./se_square)'*betavar(alpha, mu, s);
-    	lnZ                 = lnZ + intgamma(logodds, alpha) + intklbeta_rssbvsr(alpha, mu, s, sigb_square);
-        num_bt              = num_bt + 1; % stop back-tracking after ten steps
+        mtp     = 0.5*(mtp-1); % back-tracking
+        alpha3  = alpha0 - 2*mtp*alpha_r + (mtp^2)*alpha_v;
+        mu3     = mu0 - 2*mtp*mu_r + (mtp^2)*mu_v;
+        SiRiSr3 = full(SiRiS * (alpha3 .* mu3));
+
+        [alpha, mu, SiRiSr] = rss_varbvsr_update(SiRiS, sigb, logodds, betahat, se, alpha3, mu3, SiRiSr3, I);
+
+        r   = alpha .* mu;
+        lnZ = q'*r - 0.5*r'*SiRiSr - 0.5*(1./se_square)'*betavar(alpha, mu, s);
+        lnZ = lnZ + intgamma(logodds, alpha) + intklbeta_rssbvsr(alpha, mu, s, sigb_square);
+
+        num_bt = num_bt + 1;
       end
     end
 
