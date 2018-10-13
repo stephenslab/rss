d7765ea715f0ec7d8cc52be75a6b87441e45362a
Xiang Zhu <xiangzhu.nku@gmail.com>
2018-10-13 18:56:50 -0500

    update intkl calculation to allow for sigb vector

--- src_vb/intklbeta_rssbvsr.m
+++ src_vb/intklbeta_rssbvsr.m
@@ -1,10 +1,15 @@
-function I = intklbeta_rssbvsr(alpha, mu, sigma_square, sigma_beta_square)
-% USAGE: compute an integral in the variational lower bound of the marginal log-likelihood.
-% INPUT: see rss_varbvsr for details on the inputs to this function.
-% OUTPUT: the negative Kullback-Leibler divergence between the approximation and the prior of the coefficients.
-  
-  I = (sum(alpha) + alpha'*log(sigma_square/sigma_beta_square) ... 
-      - alpha'*(sigma_square + mu.^2)/sigma_beta_square) * 0.5 ...
-      - alpha'*log(alpha + eps) - (1 - alpha)'*log(1 - alpha + eps);
+function I = intklbeta_rssbvsr(alpha, mu, s, sigb_square)
+% USAGE: compute an integral in the variational lower bound of the marginal log-likelihood
+% INPUT:
+%	alpha: p by 1, variational estimates of the posterior inclusion probabilities
+%	mu: p by 1, posterior means of the additive effects (given snp included)
+%	s: p by 1, posterior variances of the additive effects (given snp included)
+%	sigb_square: p by 1 or scalar, prior variance of the regression coefficients (if included)
+% OUTPUT:
+%	I: negative KL divergence between the approximation and the prior of regression coefficients
+ 
+  I = sum(alpha) + alpha'*log(s./sigb_square);
+  I = (I - alpha'*((s+mu.^2)./sigb_square)) * 0.5;
+  I = I - alpha'*log(alpha+eps) - (1-alpha)'*log(1-alpha+eps);
 
 end  
