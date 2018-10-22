9886c8bf1c7b601a1b7f3274013fc2636ea42995
Xiang Zhu <xiangzhu.nku@gmail.com>
2018-10-22 17:27:21 -0500

    upgrade sigb from a scalar to a vector in rss-varbvsr (parallel version)

--- src_vb/rss_varbvsr_parallel.m
+++ src_vb/rss_varbvsr_parallel.m
@@ -1,27 +1,26 @@
 function [lnZ, alpha, mu, s, info] = rss_varbvsr_parallel(betahat, se, SiRiS, sigb, logodds, options)
-% USAGE: mean-field variational approximation of the RSS-BVSR model given the hyperparameters
-%	 with parfor (MATLAB Parallel Computing Toolbox) added to faciliate parallel calculations
+% USAGE: mean-field variational inference of RSS-BVSR model for a given set of hyperparameters
+%        use parfor function (MATLAB Parallel Computing Toolbox) for parallel calculations
 % INPUT:
-%       betahat: the effect size estimates under single-SNP model, C by 1 cell array
+%       betahat: effect size estimates under single-SNP model, C by 1 cell array
 %       se: standard errors of betahat, C by 1 cell array
-%       SiRiS: inv(S)*R*inv(S), double precision sparse matrix (ccs format), C by 1 cell array
-%       sigb: the prior SD of the regression coefficients (if included), scalar
-%       logodds: the prior log-odds (i.e. log(prior PIP/(1-prior PIP))) of inclusion for each SNP, p by 1 or scalar
+%       SiRiS: inv(S)*R*inv(S), double precision sparse matrix (CCS format), C by 1 cell array
+%       sigb: prior SDs of regression coefficients (if included), p by 1 or scalar
+%       logodds: log(prior PIP/(1-prior PIP)) of inclusion for each SNP, p by 1 or scalar
 %       options: user-specified behaviour of the algorithm, structure
 %               - max_walltime: scalar, the maximum wall time (unit: seconds) for this program
 %               - tolerance: scalar, convergence tolerance
 %               - alpha & mu: p by 1 vectors, initial values of variational parameters
 %               - verbose: logical, print program progress if true
 % OUTPUT:
-%       lnZ: scalar, the variational lower bound of the marginal log likelihood (up to some constant)
+%       lnZ: scalar, variational lower bound of the marginal log likelihood (up to some constant)
 %       alpha: p by 1, variational estimates of the posterior inclusion probabilities 
-%       mu: p by 1, posterior means of the additive effects (given snp included)
-%       s: p by 1, posterior variances of the additive effects (given snp included)
+%       mu: p by 1, posterior means of the additive effects (if the SNP is included)
+%       s: p by 1, posterior variances of the additive effects (if the SNP is included)
 %       info: structure with following fields 
-%               - iter: integer, number of iterations
-%               - maxerr: the maximum relative difference between the parameters at the last two iterations
-%               - sigb: scalar, the maximum likelihood estimate of sigma_beta
-%               - loglik: iter by 1, the variational lower bound at each iteration
+%               - iter: integer, number of iterations till convergence
+%               - maxerr: maximum relative difference between the parameters at the last 2 iterations
+%               - loglik: iter by 1, variational lower bound at each iteration
 
   % Get the time when the program starts.
   start_time = clock;
@@ -49,9 +48,19 @@ function [lnZ, alpha, mu, s, info] = rss_varbvsr_parallel(betahat, se, SiRiS, si
   % Get the number of analyzed SNPs in the whole genome (p).
   p = length(cell2mat(betahat));
 
-  % Convert logodds to a vector if the input is a scalar.
+  % Set the hyper-parameters (sigb and logodds).
+  if isscalar(sigb)
+    disp('Prior SDs (sigb) of all SNPs are the same.');
+    sigb = repmat(sigb,p,1);
+  end
   if isscalar(logodds)
+    disp('Prior log-odds (logodds) of all SNPs are the same.');
     logodds = repmat(logodds,p,1);
+  end
+
+  % SiRiS must be a sparse matrix.
+  if ~issparse(SiRiS)
+    SiRiS = sparse(double(SiRiS));
   end
 
   % Set initial estimates of variational parameters.
@@ -79,38 +88,40 @@ function [lnZ, alpha, mu, s, info] = rss_varbvsr_parallel(betahat, se, SiRiS, si
 
   clear options;
 
-  % Partition the whole genome into chromosomes.
-  C 	 = length(betahat);
+  % Partition the whole genome data by chromosomes.
+  C      = length(betahat);
   chrpar = partition_genome(betahat); 
 
-  lnZ_cell   	= zeros(C, 1);
-  alpha_cell 	= cell(C, 1);
-  mu_cell    	= cell(C, 1);
-  params_cell   = cell(C, 1); 
-  s_cell	= cell(C, 1);
-  SiRiSr_cell 	= cell(C, 1);
-  q_cell 	= cell(C, 1);
-  sesquare_cell	= cell(C, 1);
-  logodds_cell  = cell(C, 1);
-  sigb_square 	= sigb * sigb;
+  lnZ_cell        = zeros(C, 1);
+  alpha_cell      = cell(C, 1);
+  mu_cell         = cell(C, 1);
+  params_cell     = cell(C, 1); 
+  s_cell          = cell(C, 1);
+  SiRiSr_cell     = cell(C, 1);
+  q_cell          = cell(C, 1);
+  sesquare_cell   = cell(C, 1);
+  logodds_cell    = cell(C, 1);
+  sigb_cell       = cell(C, 1);
+  sigbsquare_cell = cell(C, 1);
 
   for c = 1:C
-    chr_start          = chrpar(c, 1); 
-    chr_end            = chrpar(c, 2);
-    alpha_cell{c, 1}   = alpha(chr_start:chr_end); 
-    mu_cell{c, 1}      = mu(chr_start:chr_end);
-    logodds_cell{c, 1} = logodds(chr_start:chr_end);
+    chr_start             = chrpar(c, 1); 
+    chr_end               = chrpar(c, 2);
+    alpha_cell{c, 1}      = alpha(chr_start:chr_end); 
+    mu_cell{c, 1}         = mu(chr_start:chr_end);
+    logodds_cell{c, 1}    = logodds(chr_start:chr_end);
+    sigb_cell{c, 1}       = sigb(chr_start:chr_end);
+    sigbsquare_cell{c, 1} = sigb(chr_start:chr_end).^2; 
   end
 
   % Compute a few useful quantities for the main loop.
   parfor c = 1:C
-    params_cell{c, 1}   = [alpha_cell{c, 1}; alpha_cell{c, 1} .* mu_cell{c, 1}];
-    SiRiSr_cell{c, 1}   = full(SiRiS{c, 1} * (alpha_cell{c, 1} .* mu_cell{c, 1}));
-    sesquare_cell{c, 1} = se{c,1} .* se{c,1};
-    q_cell{c, 1}        = betahat{c,1} ./ sesquare_cell{c,1};
-    s_cell{c, 1}        = (sesquare_cell{c, 1} .* sigb_square) ./ (sesquare_cell{c, 1} + sigb_square);
+    params_cell{c,1}   = [alpha_cell{c,1}; alpha_cell{c,1} .* mu_cell{c,1}];
+    SiRiSr_cell{c,1}   = full(SiRiS{c,1} * (alpha_cell{c,1} .* mu_cell{c,1}));
+    sesquare_cell{c,1} = se{c,1} .* se{c,1};
+    q_cell{c,1}        = betahat{c,1} ./ sesquare_cell{c,1};
+    s_cell{c,1}        = (sesquare_cell{c,1} .* sigbsquare_cell{c,1}) ./ (sesquare_cell{c,1} + sigbsquare_cell{c,1});
   end
-
 
   % Aggregate the variational estimates of variance.
   s = cell2mat(s_cell);
@@ -125,7 +136,7 @@ function [lnZ, alpha, mu, s, info] = rss_varbvsr_parallel(betahat, se, SiRiS, si
 
     lnZ_cell(c) = (q_cell{c,1})'*r - 0.5*r'*SiRiSr_cell{c,1} + intgamma(logodds_cell{c,1},alpha_cell{c,1});
     lnZ_cell(c) = lnZ_cell(c) - 0.5*(1./sesquare_cell{c,1})'*betavar(alpha_cell{c,1},mu_cell{c,1},s_cell{c,1});
-    lnZ_cell(c) = lnZ_cell(c) + intklbeta_rssbvsr(alpha_cell{c,1},mu_cell{c,1},s_cell{c,1},sigb_square);
+    lnZ_cell(c) = lnZ_cell(c) + intklbeta_rssbvsr(alpha_cell{c,1},mu_cell{c,1},s_cell{c,1},sigbsquare_cell{c,1});
   end
   lnZ = sum(lnZ_cell);
   fprintf('Calculate the variational lower bound based on the initial values: %+13.6e ...\n', lnZ);
@@ -133,8 +144,8 @@ function [lnZ, alpha, mu, s, info] = rss_varbvsr_parallel(betahat, se, SiRiS, si
   loglik = [loglik; lnZ]; 
 
   if verbose
-    fprintf('       variational    max. incl max.       \n');
-    fprintf('iter   lower bound  change vars E[b] sigma2\n');
+    fprintf('       variational    max. incl max.\n');
+    fprintf('iter   lower bound  change vars E[b]\n');
   end
 
   % Repeat until convergence criterion is met.
@@ -144,43 +155,45 @@ function [lnZ, alpha, mu, s, info] = rss_varbvsr_parallel(betahat, se, SiRiS, si
     iter = iter + 1;
     
     % Save the current variational parameters and lower bound.
-    lnZ0    	 = lnZ;
+    lnZ0         = lnZ;
     alpha0_cell  = alpha_cell; 
     mu0_cell     = mu_cell;
     params0_cell = params_cell;
     SiRiSr0_cell = SiRiSr_cell;
 
     % Parallel for loop over each chromosome.
-    maxerr_uni 	= zeros(C, 1);
-    absr_uni 	= zeros(C, 1);
-    asum_uni 	= zeros(C, 1);
+    maxerr_uni = zeros(C, 1);
+    absr_uni   = zeros(C, 1);
+    asum_uni   = zeros(C, 1);
  
     parfor c = 1:C
 
-      pchr = length(betahat{c,1}); % # of SNPs on Chr. c
+      % Get the number of SNPs on Chr. c
+      pchr = length(betahat{c,1});
 
       % All SNPs are included in the forward/backward updates.
       if mod(iter,2)
-        I = 1:pchr;
+        I = (1:pchr);
       else
-        I = pchr:-1:1;
+        I = (pchr:-1:1);
       end
 
       % Run a forward or backward pass of the coordinate ascent updates on Chr. c.
-      [alpha_tmp,mu_tmp,SiRiSr_tmp] = rss_varbvsr_update(SiRiS{c,1},sigb,logodds_cell{c,1},betahat{c,1},se{c,1}, ...
-							 alpha0_cell{c,1},mu0_cell{c,1},SiRiSr0_cell{c,1},I);
+      [alpha_tmp,mu_tmp,SiRiSr_tmp] = rss_varbvsr_update(SiRiS{c,1},sigb_cell{c,1},...
+                                      logodds_cell{c,1},betahat{c,1},se{c,1},...
+                                      alpha0_cell{c,1},mu0_cell{c,1},SiRiSr0_cell{c,1},I);
 
-      alpha_cell{c,1} 	= alpha_tmp;
-      mu_cell{c,1} 	= mu_tmp;
-      r                 = alpha_tmp .* mu_tmp;
-      params_tmp  	= [alpha_tmp; r];
-      params_cell{c,1}  = params_tmp;
-      SiRiSr_cell{c,1} 	= SiRiSr_tmp;
+      alpha_cell{c,1}  = alpha_tmp;
+      mu_cell{c,1}     = mu_tmp;
+      r                = alpha_tmp .* mu_tmp;
+      params_tmp       = [alpha_tmp; r];
+      params_cell{c,1} = params_tmp;
+      SiRiSr_cell{c,1} = SiRiSr_tmp;
 
       % Compute the lower bound to the marginal log-likelihood of Chr. c.
       lnZ_cell(c) = (q_cell{c,1})'*r - 0.5*r'*SiRiSr_cell{c,1} + intgamma(logodds_cell{c,1},alpha_cell{c,1});
       lnZ_cell(c) = lnZ_cell(c) - 0.5*(1./sesquare_cell{c,1})'*betavar(alpha_cell{c,1},mu_cell{c,1},s_cell{c,1});
-      lnZ_cell(c) = lnZ_cell(c) + intklbeta_rssbvsr(alpha_cell{c,1},mu_cell{c,1},s_cell{c,1},sigb_square);
+      lnZ_cell(c) = lnZ_cell(c) + intklbeta_rssbvsr(alpha_cell{c,1},mu_cell{c,1},s_cell{c,1},sigbsquare_cell{c,1});
 
       % Compute some quantities related to the stopping rule.
       J_tmp         = find(abs(params_tmp) > 1e-6);
@@ -208,7 +221,7 @@ function [lnZ, alpha, mu, s, info] = rss_varbvsr_parallel(betahat, se, SiRiS, si
     % parameters that are very small.
     
     if verbose
-      status = sprintf('%4d %+13.6e %0.1e %4d %0.2f %5.2f',iter,lnZ,maxerr,asum,absr,sigb_square);
+      status = sprintf('%4d %+13.6e %0.1e %4d %0.2f',iter,lnZ,maxerr,asum,absr);
       fprintf(status);
       fprintf(repmat('\b',1,length(status)));
     end
@@ -222,14 +235,12 @@ function [lnZ, alpha, mu, s, info] = rss_varbvsr_parallel(betahat, se, SiRiS, si
       alpha = cell2mat(alpha0_cell);
       mu    = cell2mat(mu0_cell);
       lnZ   = lnZ0;
-      sigb  = sqrt(sigb_square);
       break
 
     elseif maxerr < tolerance
 
       alpha = cell2mat(alpha_cell);
       mu    = cell2mat(mu_cell);
-      sigb  = sqrt(sigb_square);
       if verbose
         fprintf('\n');
         fprintf('Convergence reached: maximum relative error %+0.2e\n',maxerr);
@@ -245,7 +256,6 @@ function [lnZ, alpha, mu, s, info] = rss_varbvsr_parallel(betahat, se, SiRiS, si
 
       alpha = cell2mat(alpha_cell);
       mu    = cell2mat(mu_cell);
-      sigb  = sqrt(sigb_square);
       if verbose
         fprintf('\n');
         fprintf('Maximum wall time reached: %+0.2e seconds\n',exetime);
@@ -258,6 +268,6 @@ function [lnZ, alpha, mu, s, info] = rss_varbvsr_parallel(betahat, se, SiRiS, si
   end
 
   % Save info as a structure array.
-  info = struct('iter',iter,'maxerr',maxerr,'sigb',sigb,'loglik',loglik);
+  info = struct('iter',iter,'maxerr',maxerr,'loglik',loglik);
 end
 
