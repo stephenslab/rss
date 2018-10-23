0a58b54448705a9c82188abdbf3f1b6b1f073e30
Xiang Zhu <xiangzhu.nku@gmail.com>
2018-10-23 16:59:53 -0500

    update docs in wrapper

--- src_vb/rss_varbvsr_wrapper.m
+++ src_vb/rss_varbvsr_wrapper.m
@@ -1,39 +1,38 @@
-function [lnZ, alpha, mu, s, info] = rss_varbvsr_wrapper(method, betahat, se, SiRiS, sigb, logodds, options)
-% USAGE: a wrapper for the mean-field variational approximation of the RSS-BVSR model given the hyperparameters
+function [lnZ,alpha,mu,s,info] = rss_varbvsr_wrapper(method,betahat,se,SiRiS,sigb,logodds,options)
+% USAGE: mean-field variational inference of RSS-BVSR model for a given set of hyperparameters
 % INPUT:
-%       method: the implementation of rss-varbvsr, character
-%       betahat: the effect size estimates under single-SNP model, p by 1 array or C by 1 cell array
+%       method: the implementation of rss-varbvsr, string
+%       betahat: effect size estimates under single-SNP model, p by 1 array or C by 1 cell array
 %       se: standard errors of betahat, p by 1 array or C by 1 cell array
-%       SiRiS: inv(S)*R*inv(S), double precision sparse matrix (ccs format), p by p array or C by 1 cell array
-%       sigb: the prior SD of the regression coefficients (if included), scalar
-%       logodds: the prior log-odds (i.e. log(prior PIP/(1-prior PIP))) of inclusion for each SNP, p by 1
-%       options: user-specified behaviour of the algorithm, structure
+%       SiRiS: inv(S)*R*inv(S), sparse matrix (CCS format), p by p array or C by 1 cell array
+%       sigb: prior SDs of the regression coefficients (if included), p by 1 or scalar
+%       logodds: log(prior PIP/(1-prior PIP)) of inclusion for each SNP, p by 1 or scalar
+%       options: user-specified behaviour for each implementation, structure
 % OUTPUT:
-%       lnZ: scalar, the variational lower bound of the marginal log likelihood (up to some constant)
+%       lnZ: scalar, variational lower bound of the marginal log likelihood (up to some constant)
 %       alpha: p by 1, variational estimates of the posterior inclusion probabilities 
-%       mu: p by 1, posterior means of the additive effects (given snp included)
-%       s: p by 1, posterior variances of the additive effects (given snp included)
+%       mu: p by 1, posterior means of the additive effects (if the SNP is included)
+%       s: p by 1, posterior variances of the additive effects (if the SNP is included)
 %       info: structure with following fields 
-%               - iter: integer, number of iterations
-%               - maxerr: the maximum relative difference between the parameters at the last two iterations
-%               - sigb: scalar, the maximum likelihood estimate of sigma_beta
-%               - loglik: iter by 1, the variational lower bound at each iteration
+%               - iter: integer, number of iterations till convergence
+%               - maxerr: maximum relative difference between the parameters at the last 2 iterations
+%               - loglik: iter by 1, variational lower bound at each iteration
 
 % IMPLEMENTATION METHODS
-% 'original': the summary data must be p-by-1 vectors and p-by-p matrix
-% 'squarem': the summary data must be p-by-1 vectors and p-by-p matrix; use SQUAREM add-on
-% 'parallel': the summary data must be C-by-1 cell arrays; use parallel for loop
-% 'pasquarem': the summary data must be C-by-1 cell arrays; use parallel for loop and SQUAREM
+% 'original': input data must be p-by-1 vectors and p-by-p matrix
+% 'squarem': input data must be p-by-1 vectors and p-by-p matrix; only use SQUAREM
+% 'parallel': input data must be C-by-1 cell arrays; only use parallel for loop
+% 'pasquarem': input data must be C-by-1 cell arrays; use both parallel for loop and SQUAREM
 
-  % make sure summary-level data are stored as C by 1 cell arrays
+  % check whether input data are stored as C by 1 cell arrays
   cell_check = prod([iscell(betahat),iscell(se),iscell(SiRiS)]);
   if cell_check == 1
-    fprintf('Summary-level data are stored as cell arrays. \n');
+    fprintf('Input data are stored as cell arrays. \n');
   end
 
   C = length(betahat);
   if prod([length(se)==C,length(SiRiS)==C]) == 0
-    error('Summary-level data must have the same dimension.');
+    error('Input data must have consistent dimensions.');
   end
 
   % decide the implementation type of rss-varbvsr
@@ -41,7 +40,7 @@ function [lnZ, alpha, mu, s, info] = rss_varbvsr_wrapper(method, betahat, se, Si
 
     case 'original'
       if C ~= 1 && cell_check == 1
-        error('Use parallel implementations.');
+        error('Use parallel implementations instead.');
       end
       if cell_check == 1
 	betahat = cell2mat(betahat);
@@ -54,7 +53,7 @@ function [lnZ, alpha, mu, s, info] = rss_varbvsr_wrapper(method, betahat, se, Si
 
     case 'squarem'
       if C ~= 1 && cell_check == 1
-	error('Use parallel implementations.');
+	error('Use parallel implementations instead.');
       end
       if cell_check == 1
         betahat = cell2mat(betahat);
@@ -62,12 +61,12 @@ function [lnZ, alpha, mu, s, info] = rss_varbvsr_wrapper(method, betahat, se, Si
         SiRiS   = cell2mat(SiRiS);
       end
 
-      fprintf('The serial implementation with SQUAREM add-on of rss-varbvsr is used.\n');
+      fprintf('The serial SQUAREM implementation of rss-varbvsr is used.\n');
       [lnZ,alpha,mu,s,info] = rss_varbvsr_squarem(betahat,se,SiRiS,sigb,logodds,options);
 
     case 'parallel'
       if cell_check ~= 1
-        error('Use serial implementations.');
+        error('Use serial implementations instead.');
       end
 
       fprintf('The parallel implementation of rss-varbvsr is used.\n');
@@ -75,10 +74,10 @@ function [lnZ, alpha, mu, s, info] = rss_varbvsr_wrapper(method, betahat, se, Si
 
     case 'pasquarem'
       if cell_check ~= 1
-        error('Use serial implementations.');
+        error('Use serial implementations instead.');
       end
 
-      fprintf('The parallel implementation with SQUAREM add-on of rss-varbvsr is used.\n');
+      fprintf('The parallel SQUAREM implementation of rss-varbvsr is used.\n');
       [lnZ,alpha,mu,s,info] = rss_varbvsr_pasquarem(betahat,se,SiRiS,sigb,logodds,options);
 
     otherwise
