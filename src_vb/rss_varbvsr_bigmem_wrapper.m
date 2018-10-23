a96ea77259390e935cea436d3baf6c632362fe54
Xiang Zhu <xiangzhu.nku@gmail.com>
2018-10-23 17:07:59 -0500

    update docs in bigmem wrapper

--- src_vb/rss_varbvsr_bigmem_wrapper.m
+++ src_vb/rss_varbvsr_bigmem_wrapper.m
@@ -1,22 +1,21 @@
 function [lnZ, alpha, mu, s, info] = rss_varbvsr_bigmem_wrapper(method, file, sigb, logodds, options)
-% USAGE: a wrapper for the mean-field variational approximation of the RSS-BVSR model given the hyperparameters
-%	 this function includes all the implementations that deal with the datasets requiring large memory
+% USAGE: mean-field variational inference of RSS-BVSR model for a given set of hyperparameters
+%        this function includes all implementations that deal with datasets requiring large memory
 % INPUT:
-%       method: the implementation of rss-varbvsr, character
-%       file: the path of mat file that contains cell arrays of betahat, se and SiRiS, string
-%       sigb: the prior SD of the regression coefficients (if included), scalar
-%       logodds: the prior log-odds (i.e. log(prior PIP/(1-prior PIP))) of inclusion for each SNP, p by 1
-%       options: user-specified behaviour of the algorithm, structure
+%       method: the implementation of rss-varbvsr, string
+%       file: the mat file that contains cell arrays of betahat, se and SiRiS, string
+%       sigb: prior SDs of the regression coefficients (if included), p by 1 or scalar
+%       logodds: log(prior PIP/(1-prior PIP)) of inclusion for each SNP, p by 1 or scalar
+%       options: user-specified behaviour for each implementation, structure
 % OUTPUT:
-%       lnZ: scalar, the variational lower bound of the marginal log likelihood (up to some constant)
+%       lnZ: scalar, variational lower bound of the marginal log likelihood (up to some constant)
 %       alpha: p by 1, variational estimates of the posterior inclusion probabilities 
-%       mu: p by 1, posterior means of the additive effects (given snp included)
-%       s: p by 1, posterior variances of the additive effects (given snp included)
+%       mu: p by 1, posterior means of the additive effects (if the SNP is included)
+%       s: p by 1, posterior variances of the additive effects (if the SNP is included)
 %       info: structure with following fields 
-%               - iter: integer, number of iterations
-%               - maxerr: the maximum relative difference between the parameters at the last two iterations
-%               - sigb: scalar, the maximum likelihood estimate of sigma_beta
-%               - loglik: iter by 1, the variational lower bound at each iteration
+%               - iter: integer, number of iterations till convergence
+%               - maxerr: maximum relative difference between the parameters at the last 2 iterations
+%               - loglik: iter by 1, variational lower bound at each iteration
 
 % IMPLEMENTATION METHODS
 % 'original': use the original parallel implementation of rss-varbvsr
@@ -30,7 +29,7 @@ function [lnZ, alpha, mu, s, info] = rss_varbvsr_bigmem_wrapper(method, file, si
       [lnZ,alpha,mu,s,info] = rss_varbvsr_bigmem(file,sigb,logodds,options);
 
     case 'squarem'
-      fprintf('The parallel bigmem implementation with SQUAREM add-on of rss-varbvsr is used.\n');
+      fprintf('The parallel bigmem SQUAREM implementation of rss-varbvsr is used.\n');
       [lnZ,alpha,mu,s,info] = rss_varbvsr_bigmem_squarem(file,sigb,logodds,options);
 
     otherwise
