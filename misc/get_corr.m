3841d321aad98d52936ea774a9f8fc0083fb8aa9
Xiang Zhu <xiangzhu.nku@gmail.com>
2018-12-13 17:22:42 -0600

    allow for negative genetic distance after listing to a newer build

--- misc/get_corr.m
+++ misc/get_corr.m
@@ -1,4 +1,4 @@
-function [R, BR] = get_corr(m, Ne, cummap, Hpanel, cutoff, isgeno)
+function [R, BR] = get_corr(m, Ne, cummap, Hpanel, cutoff, isgeno, negcm)
 % USAGE: compute LD matrix using the shrinkage estimator in Wen and Stephens (2010)
 % INPUT:
 %	m: the number of individuals in the reference panel, integer
@@ -7,6 +7,7 @@ function [R, BR] = get_corr(m, Ne, cummap, Hpanel, cutoff, isgeno)
 %	Hpanel: the (phased) haplotypes from a reference panel, numIND by numSNP
 %	cutoff: the hard threshold for small entries being zero, scalar 
 %	isgeno: true if Hpanel is an unphased genotype matrix, logical
+%	negcm: true if there is negative genetic distance, logical
 % OUTPUT:
 %	R: the estimated LD matrix, numSNP by numSNP, sparse matrix
 %	BR: the banded storage of R, dense matrix
@@ -16,9 +17,26 @@ function [R, BR] = get_corr(m, Ne, cummap, Hpanel, cutoff, isgeno)
     isgeno = false;
   end
 
+  if isgeno
+    disp('Input genetic data are genotypes.');
+  else
+    disp('Input genetic data are haplotypes.');
+  end
+
+  % decide whether there exists negative genetic distance or not
+  if ~exist('negcm', 'var')
+    negcm = false;
+  end
+
+  if negcm
+    disp('Negative genetic distance is allowed here.');
+  else
+    disp('Negative genetic distance is not allowed here.');
+  end
+
   % compute the shrinkage estimator of covariance matrix
   disp('Compute Wen-Stephens shrinkage LD estimator ...'); 
-  SigHat = shrink_cov(m, Ne, cummap, Hpanel, cutoff, isgeno);
+  SigHat = shrink_cov(m, Ne, cummap, Hpanel, cutoff, isgeno, negcm);
 
   % convert covariance to correlation
   R = corrcov(SigHat);
@@ -38,7 +56,7 @@ function [R, BR] = get_corr(m, Ne, cummap, Hpanel, cutoff, isgeno)
   R = sparse(R);
 end
 
-function SigHat = shrink_cov(m, Ne, cummap, Hpanel, cutoff, isgeno)
+function SigHat = shrink_cov(m, Ne, cummap, Hpanel, cutoff, isgeno, negcm)
 % USAGE: compute the shrinkage estimator of covariance matrix in Wen and Stephens (2010)
 % INPUT:
 %	m: number of individuals in a reference panel, integer
@@ -46,7 +64,8 @@ function SigHat = shrink_cov(m, Ne, cummap, Hpanel, cutoff, isgeno)
 %	cummap: cumulative genetic map in cM, numSNP by 1
 %	Hpanel: phased haplotypes or unphased genotypes in a reference panel, numIND by numSNP
 %	cutoff: hard threshold for small entries being zero, scalar
-%	isgeno: true if Hpanel is an unphased genotype matrix, logical 
+%	isgeno: true if Hpanel is an unphased genotype matrix, logical
+%	negcm: true if there is negative genetic distance, logical 
 % OUTPUT:
 %	SigHat: estimated covariance matrix of haplotype, numSNP by numSNP
 
@@ -69,25 +88,46 @@ function SigHat = shrink_cov(m, Ne, cummap, Hpanel, cutoff, isgeno)
 
   % compute the values on and above the 1st diagonal
   numSNP = size(S,1);
+
   for i = 1:numSNP
     for j = (i+1):numSNP
-      if cummap(j) < cummap(i)
-        error('Negative recombination rate is produced.')
+
+      % compute genetic distance between SNPs i and j
+      genetic_dist = cummap(j) - cummap(i);
+
+      % deal with potential negative distance value
+      % which may occur after lifting genome builds
+      if (genetic_dist<0) & (~negcm)
+        error('Negative recombination rate is not allowed.')
       end
-      rho = 4 * Ne * (cummap(j) - cummap(i)) / 100;
+
+      if (genetic_dist<0) & (negcm)
+        genetic_dist = abs(genetic_dist);
+      end
+
+      % compute scaled population recombination rate
+      % see https://stephenslab.github.io/rss/Recombination
+      rho = 4 * Ne * genetic_dist / 100;
+
+      % compute the shrinkage factor based on recombination rate
       shrinkage = exp(-rho/(2*m));
-      % hard thresholding to obtain sparse and banded estimator
+
+      % apply hard thresholding to obtain sparse and banded estimator
       if shrinkage < cutoff
         shrinkage = 0;
       end
-      S(i, j) = shrinkage * S(i, j);
+
+      % shrink the sample covariance in the reference panel
+      S(i,j) = shrinkage * S(i,j);
+
     end
   end
 
   % copy the upper half (NO diagonal) to the lower half
-  S = S + triu(S, 1)';
+  S = S + triu(S,1)';
 	
   % SigHat is derived from Li and Stephens model (2003)
   SigHat = (1-theta)^2 * S + 0.5*theta * (1-0.5*theta) * eye(numSNP);
+
 end
 
